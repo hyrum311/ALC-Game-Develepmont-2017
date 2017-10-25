ab0317b306f7db75d3c45e44a23cc8753d92bf68
Unknown <hyrum311@gmail.com>
2017-10-25 13:10:31 -0600

    Made house flashligh almost turns off and on

---
+++ Ghost house!/Assets/Scripts/LighOn-Off.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FlashLight : MonoBehaviour {
+
+
+	public bool lightOn;
+	light light;
+	void Start () {
+		light = GetComponent<Light> ();
+
+		lightOn = true;
+		light.enabled = true;
+	
+	}
+	
+	
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.L) && lightOn){
+			lightOn = false;
+			light.enabled = false;
+
+		}
+		else if (Input.GetKeyUp (KeyCode.L) && !lightOn)
+		{
+			lightOn = true;
+			light.enabled=true;
+		}
+	
+	}
+}
