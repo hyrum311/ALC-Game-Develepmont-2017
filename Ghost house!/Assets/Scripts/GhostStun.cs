bc163a06defd0b0828531a1cf899daf0c960964a
Unknown <hyrum311@gmail.com>
2017-11-02 13:08:43 -0600

    Ghost Stun work on house

---
+++ Ghost house!/Assets/Scripts/GhostStun.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+ public class GhostStun : MonoBehaviour {
+
+ //bool lightCheck;
+	
+ 	void Start () {
+ 		//lightCheck = GetComponent<FlashLight>().LightOn;
+ 	}
+	
+	
+ 	void Update () {
+	
+ 	} 	
+	 void OnTriggerEnter(Collider other){
+ 		
+ 		if(other.gameObject.name == "Ghost") {
+
+ 			print("Ghost is Stunned!"); 
+ 		}
+	}
+ }
